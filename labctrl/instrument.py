c75c6805743f60a4ef1fa5d017c9f87822677613
Atharv Joshi <atharvjoshi@ymail.com>
2021-11-13 23:05:01 +0800

    Write DLLInstrument and VISAInstrument classes

--- labctrl/instrument.py
+++ labctrl/instrument.py
@@ -1,10 +1,18 @@
 """ """
 
+import ctypes
+import inspect
+from pathlib import Path
 from typing import Any
 
+import pyvisa
 import yaml
 
-from parameter import Parameter, parametrize
+from labctrl.parametrizer import Param, parametrize
+
+
+class InstrumentConnectionError(Exception):
+    """ """
 
 
 class InstrumentMetaclass(type):
@@ -12,43 +20,35 @@ class InstrumentMetaclass(type):
 
     def __init__(cls, name, bases, kwds) -> None:
         super().__init__(name, bases, kwds)
+        cls._params: set[str] = parametrize(cls)
         yaml.SafeDumper.add_representer(cls, cls.dump)
         yaml.SafeLoader.add_constructor(name, cls.load)
 
     def __repr__(cls) -> str:
         return f"<class '{cls.__name__}>"
 
+    @property
+    def params(cls) -> set[str]:
+        """ """
+        return cls._params
+
 
 class Instrument(metaclass=InstrumentMetaclass):
     """ """
 
-    name: str = Parameter(setter=False)
-    id: Any = Parameter(setter=False)
+    name = Param(bounds=str, setter=False)
+    id = Param(setter=False)
 
-    def __init__(self, name: str, id: Any, **parameters) -> None:
+    def __init__(self, name: str, id: Any) -> None:
         """ """
-        self._parameters: set[str] = parametrize(self.__class__)
         self.name = name
         self.id = id
-        self._handle = self.connect()
-        if parameters:
-            self.configure(**parameters)
 
     def __repr__(self) -> str:
         """ """
         return f"{self.name}-{self.id}"
 
-    @property
-    def handle(self) -> Any:
-        """ """
-        return self._handle
-
-    @property
-    def parameters(self) -> set[str]:
-        """ """
-        return self._parameters
-
-    def connect(self) -> Any:
+    def connect(self) -> None:
         """ """
         raise NotImplementedError("Instrument subclasses must implement connect()")
 
@@ -56,27 +56,70 @@ class Instrument(metaclass=InstrumentMetaclass):
         """ """
         raise NotImplementedError("Instrument subclasses must implement disconnect()")
 
-    def configure(self, **parameters) -> None:
+    def configure(self, **params) -> None:
         """ """
-        for name, value in parameters.items():
-            if name not in self._parameters:
-                message = f"'{name}' is not a parameter of {self}: {self._parameters}"
+        for name, value in params.items():
+            if name not in self.params:
+                message = f"'{name}' is not a param of {self}: {self.params}"
                 raise AttributeError(message)
             setattr(self, name, value)
 
     def snapshot(self) -> dict[str, Any]:
         """ """
-        return {name: getattr(self, name) for name in self._parameters}
+        return {name: getattr(self, name) for name in self.params}
 
     @classmethod
     def dump(cls, dumper: yaml.SafeDumper, instrument) -> yaml.MappingNode:
         """ """
-        yaml_tag = instrument.__class__.__name__
-        yaml_map = instrument.snapshot()
-        return dumper.represent_mapping(yaml_tag, yaml_map)
+        yamltag = instrument.__class__.__name__
+        yamlmap = instrument.snapshot()
+        return dumper.represent_mapping(yamltag, yamlmap)
 
     @classmethod
     def load(cls, loader: yaml.SafeLoader, node: yaml.MappingNode):
         """ """
-        parameters = loader.construct_mapping(node, deep=True)
-        return cls(**parameters)
+        params = loader.construct_mapping(node, deep=True)
+        return cls(**params)
+
+
+class DLLInstrument(Instrument):
+    """ """
+
+    def __init__(self, name: str, id: Any, **params) -> None:
+        """ """
+        super().__init__(name, id)
+        modulepath = Path(inspect.getsourcefile(self.__class__))
+        handlepath = modulepath.parent / f"{modulepath.stem}.dll"
+        self._handle = ctypes.CDLL(str(handlepath))
+        self._pointer = self.connect()
+        self.configure(**params)
+
+    @property
+    def handle(self) -> tuple[Any]:
+        """ """
+        return self._handle, self._pointer
+
+
+class VISAInstrument(Instrument):
+    """ """
+
+    id = Param(bounds=str, setter=False)
+
+    def __init__(self, name: str, id: Any, **params) -> None:
+        """ """
+        super().__init__(name, id)
+        self._handle = self.connect()
+        self.configure(**params)
+
+    @property
+    def handle(self) -> tuple[Any]:
+        """ """
+        return self._handle
+
+    def connect(self) -> None:
+        """ """  # TODO error handling
+        return pyvisa.ResourceManager().open_resource(self.id)
+
+    def disconnect(self) -> None:
+        """ """
+        self._handle.close()
