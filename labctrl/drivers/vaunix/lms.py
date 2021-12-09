cbbc5dd32dd38b6050ba571ddfa5f4c8756c7d5e
Atharv Joshi <atharvjoshi@ymail.com>
2021-12-09 12:33:21 +0800

    make LabBrick self-contained

--- labctrl/drivers/vaunix/lms.py
+++ labctrl/drivers/vaunix/lms.py
@@ -15,21 +15,30 @@ DLL = CDLL(str(Path(__file__).parent / "lms.dll"))  # driver
 class LabBrick(Instrument):
     """ """
 
-    id = Parameter(bounds=int)
+    rf = Parameter(bounds=(bool, {0, 1}))
 
-    rf = Parameter(bounds=bool)
+    min_frequency = Parameter()
+    max_frequency = Parameter()
+    _unit_frequency = 10.0  # LMS encodes frequency as an integer of 10Hz steps
+    _to_frequency = lambda value: value * LabBrick._unit_frequency
+    _from_frequency = lambda frequency: int(frequency / LabBrick._unit_frequency)
+    _check_frequency = lambda value, lb: lb.min_frequency <= value <= lb.max_frequency
+    frequency = Parameter(bounds=[Real, _check_frequency])
 
-    check_frequency = lambda value, lb: lb.min_frequency <= value <= lb.max_frequency
-    frequency = Parameter(bounds=[Real, check_frequency])
-
-    check_power = lambda value, lb: lb.min_power <= value <= lb.max_power
-    power = Parameter(bounds=[Real, check_power])
+    min_power = Parameter()
+    max_power = Parameter()
+    _unit_power = 0.25  # LMS encodes power level as an integer of 0.25dB steps
+    _to_power = lambda value: value * LabBrick._unit_power
+    _from_power = lambda power: int(power / LabBrick._unit_power)
+    _check_power = lambda value, lb: lb.min_power <= value <= lb.max_power
+    power = Parameter(bounds=[Real, _check_power])
 
     def __init__(self, name: str, id: int, **parameters) -> None:
         """ """
         super().__init__(name, id, **parameters)
         DLL.fnLMS_SetTestMode(False)  # we are using actual hardware
         DLL.fnLMS_SetUseInternalRef(self._handle, False)  # use external 10MHz reference
+        self.idle()
 
     def connect(self) -> None:
         """ """
@@ -40,13 +49,15 @@ class LabBrick(Instrument):
                 raise ConnectionError(f"please reconnect {self} at USB port") from None
 
         numdevices = DLL.fnLMS_GetNumDevices()
-        device_info = DLL.fnLMS_GetDevInfo((c_int * numdevices)())
-        ids = [DLL.fnLMS_GetSerialNumber(device_info[i]) for i in range(numdevices)]
+        deviceinfo = (c_int * numdevices)()
+        DLL.fnLMS_GetDevInfo(deviceinfo)
+        ids = [DLL.fnLMS_GetSerialNumber(deviceinfo[i]) for i in range(numdevices)]
         if self.id in ids:
-            handle = device_info[ids.index(self.id)]
-            status = DLL.fnLMS_InitDevice(handle)
-            if status == 0:  # 0 indicates successful device initialization
+            handle = deviceinfo[ids.index(self.id)]
+            error = DLL.fnLMS_InitDevice(handle)
+            if not error:  # 0 indicates successful device initialization
                 self._handle = handle
+                return
             raise ConnectionError(f"failed to open {self}")
         raise ConnectionError(f"could not find {self}")
 
@@ -67,60 +78,52 @@ class LabBrick(Instrument):
         if errorcode:  # non-zero return values indicate error
             raise ConnectionError(f"got {errorcode = } from {self}")
 
-    @rf.getter(parser=bool)
-    def rf(self) -> bool:
+    @rf.getter
+    def rf(self) -> int:
         """ """
         return DLL.fnLMS_GetRF_On(self._handle)
 
     @rf.setter
     def rf(self, value: bool) -> None:
         """ """
-        DLL.fnLMS_SetRFOn(self._handle, value)
+        self._errorcheck(DLL.fnLMS_SetRFOn(self._handle, value))
 
-    unit_frequency = 10.0  # LMS encodes frequency as an integer of 10Hz steps
-    to_frequency = lambda value: value * unit_frequency
-    from_frequency = lambda frequency: int(frequency / unit_frequency)
-
-    @property
+    @min_frequency.getter(parser=_to_frequency)
     def min_frequency(self) -> float:
         """ """
-        return self.to_frequency(DLL.fnLMS_GetMinFreq(self._handle))
+        return DLL.fnLMS_GetMinFreq(self._handle)
 
-    @property
+    @max_frequency.getter(parser=_to_frequency)
     def max_frequency(self) -> float:
         """ """
-        return self.to_frequency(DLL.fnLMS_GetMaxFreq(self._handle))
+        return DLL.fnLMS_GetMaxFreq(self._handle)
 
-    @frequency.getter(parser=to_frequency)
+    @frequency.getter(parser=_to_frequency)
     def frequency(self) -> float:
         """ """
         return DLL.fnLMS_GetFrequency(self._handle)
 
-    @frequency.setter(parser=from_frequency)
+    @frequency.setter(parser=_from_frequency)
     def frequency(self, value: Real) -> None:
         """ """
         self._errorcheck(DLL.fnLMS_SetFrequency(self._handle, value))
 
-    unit_power = 0.25  # LMS encodes power level as an integer of 0.25dB steps
-    to_power = lambda value: value * unit_power
-    from_power = lambda power: int(power / unit_power)
-
-    @property
+    @min_power.getter(parser=_to_power)
     def min_power(self) -> float:
         """ """
-        return self.to_power(DLL.fnLMS_GetMinPwr(self._handle))
+        return DLL.fnLMS_GetMinPwr(self._handle)
 
-    @property
+    @max_power.getter(parser=_to_power)
     def max_power(self) -> float:
         """ """
-        return self.to_power(DLL.fnLMS_GetMaxPwr(self._handle))
+        return DLL.fnLMS_GetMaxPwr(self._handle)
 
-    @power.getter(parser=to_power)
+    @power.getter(parser=_to_power)
     def power(self) -> float:
         """ """
         return DLL.fnLMS_GetAbsPowerLevel(self._handle)
 
-    @power.setter(parser=from_power)
+    @power.setter(parser=_from_power)
     def power(self, value: Real) -> None:
         """ """
         self._errorcheck(DLL.fnLMS_SetPowerLevel(self._handle, value))
