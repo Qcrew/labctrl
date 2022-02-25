b4204ea354301551be2e5ca34e562e56bacd4008
Atharv Joshi <atharvjoshi@ymail.com>
2022-02-25 22:38:24 +0800

    Better error handling for `load()`

--- labctrl/stage.py
+++ labctrl/stage.py
@@ -7,15 +7,24 @@ from pathlib import Path
 
 import yaml
 
+
 class StagingError(Exception):
     """ """
 
+
 def load(configpath: Path) -> list:
     """returns a list of resource objects by reading a YAML file e.g. Instruments"""
-    # IO error, attribute error, and bad yaml tag error should be avoided thru careful GUI construction. The only error we have to deal with is if the user selects a BAD configpath (non-yaml file, yaml file with unrecognized tags) to load resources from
     try:
         with open(configpath, mode="r") as config:
             return yaml.safe_load(config)
+    except AttributeError:
+        message = (
+            f"Failed to load a labctrl resource from {configpath}\n"
+            f"An entry in {configpath.name} may have an invalid attribute (key)"
+        )
     except yaml.YAMLError:
-        message = f"Failed to identify and load labctrl resources from {configpath}"
+        message = (
+            f"Failed to identify and load labctrl resources from {configpath}\n"
+            f"{configpath.name} may have an invalid or unrecognized yaml tag"
+        )
         raise StagingError(message) from None
