2bd27c7892dca494688d08a1a9c6a1cb77349f04
Atharv Joshi <atharvjoshi@ymail.com>
2022-04-05 01:45:15 +0800

    Ensure existence of settings.yml with default settings

--- labctrl/settings.py
+++ labctrl/settings.py
@@ -8,8 +8,15 @@ import yaml
 
 from labctrl.logger import logger
 
-SETTINGSPATH = Path(__file__).resolve().parent / "settings.yml"
+# set of all labctrl settings
+SETTINGS = {"datapath", "resourcepath"}
 
+SETTINGSPATH = Path(__file__).resolve().parent / "settings.yml"
+SETTINGSPATH.touch(exist_ok=True)  # ensure an empty settings.yml always exists
+DEFAULT_SETTINGS = dict.fromkeys(SETTINGS)
+with open(SETTINGSPATH, "r+") as config:
+    if yaml.safe_load(config) is None:
+        yaml.safe_dump(DEFAULT_SETTINGS, config)  # ensure default settings always exist
 
 class Settings:
     """ 
@@ -17,29 +24,24 @@ class Settings:
     How to use:
     
     with Settings() as settings:
-        print(settings.settings)  # to get a list of all available settings
-        settings.<setting1> = <value1>
-        settings.<setting2> = <value2>
+        print(settings)  # to get all labctrl settings that a user can specify
+        settings.<setting1> = <value1>  # set 'setting1' to 'value1'
+        settings.<setting2> = <value2>  # set 'setting2' to 'value2'
         ...
-
-    The logger will also print all available settings after initializing Settings().
     """
-
-    _settings: set[str] = {"configpath", "datapath", "resourcepath"}
 
     def __init__(self) -> None:
         """ """
-        logger.info(f"Available labctrl settings: {Settings._settings}")
+        with open(SETTINGSPATH, "r") as config:
+            self._settings = yaml.safe_load(config)            
 
-        if SETTINGSPATH.exists():
-            with open(SETTINGSPATH, "r") as config:
-                settings = yaml.safe_load(config)
-        else:
-            settings = dict.fromkeys(Settings._settings)  # default settings
-
-        for name, value in settings.items():
+        for name, value in self._settings.items():
             setattr(self, name, value)
-            logger.info(f"Found labctrl setting '{name}' = '{value}'.")
+            logger.debug(f"Found labctrl setting '{name}' = '{value}'.")
+
+    def __repr__(self) -> str:
+        """ """
+        return f"{self.__class__.__name__} = {SETTINGS}"
 
     def __enter__(self) -> Settings:
         """ """
@@ -47,12 +49,18 @@ class Settings:
 
     def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
         """ """
-        settings = {}
-        for name in Settings._settings:
-            value = getattr(self, name, None)
-            settings[name] = value
-            logger.info(f"Saving labctrl setting '{name}' = '{value}'...")
-        
+        self.save()
+
+    @property
+    def settings(self) -> dict[str, str]:
+        """ """
+        return {setting: getattr(self, setting) for setting in SETTINGS}
+
+    def save(self) -> None:
+        """ """
+        settings = self.settings
+        logger.debug(f"Saving labctrl settings = {self.settings}...")
+
         with open(SETTINGSPATH, "w+") as config:
             try:
                 yaml.safe_dump(settings, config)
@@ -62,9 +70,4 @@ class Settings:
                     f"Details: {error}"
                     f"Reverted to default settings. Please fix the error and try again."
                 )
-                yaml.safe_dump(dict.fromkeys(Settings._settings), config)  # default
-
-    @property
-    def settings(self) -> list[str]:
-        """ """
-        return sorted(Settings._settings)
+                yaml.safe_dump(DEFAULT_SETTINGS, config)
