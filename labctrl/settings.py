5a8346909cebc58f0e444401b2bd985b7b3497ee
Atharv Joshi <atharvjoshi@ymail.com>
2022-04-04 16:35:59 +0800

    Make settings a context manager

--- labctrl/settings.py
+++ labctrl/settings.py
@@ -1,5 +1,7 @@
 """ Control global path settings for labctrl """
 
+from __future__ import annotations
+
 from pathlib import Path
 
 import yaml
@@ -11,35 +13,46 @@ SETTINGSPATH = Path(__file__).resolve().parent / "settings.yml"
 
 class Settings:
     """ 
+    Context manager for settings
     How to use:
-        settings = Settings()
-        settings.<setting> = <value>
+    
+    with Settings() as settings:
+        print(settings.settings)  # to get a list of all available settings
+        settings.<setting1> = <value1>
+        settings.<setting2> = <value2>
         ...
-        settings.save()
-    Run 'settings.settings' to get a list of all settable <setting> attributes
+
+    The logger will also print all available settings after initializing Settings().
     """
 
-    _settings: set[str] = {"configpath", "datapath", "logspath", "resourcepath"}
+    _settings: set[str] = {"configpath", "datapath", "resourcepath"}
 
     def __init__(self) -> None:
         """ """
-        with open(SETTINGSPATH, "r") as config:
-            settings = yaml.safe_load(config)
+
+        if SETTINGSPATH.exists():
+            with open(SETTINGSPATH, "r") as config:
+                settings = yaml.safe_load(config)
+        else:
+            settings = dict.fromkeys(Settings._settings)
 
         for name, value in settings.items():
             setattr(self, name, value)
             logger.info(f"Found labctrl setting '{name}' = '{value}'.")
+            if value is None:
+                logger.info(f"Please set {name} as it is currently set to '{value}'.")
+
+    def __enter__(self) -> Settings:
+        """ """
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback) -> None:
+        """ """
+        settings = {k: v for k, v in self.__dict__.items() if k in self.settings}
+        with open(SETTINGSPATH, "w+") as config:
+            yaml.safe_dump(settings, config)
 
     @property
     def settings(self) -> list[str]:
         """ """
         return sorted(Settings._settings)
-
-    def save(self) -> None:
-        """ """
-        settings = {}
-        for key in Settings._settings:
-            value = getattr(self, key, None)
-            settings[key] = value
-        with open(SETTINGSPATH, "w+") as config:
-            yaml.safe_dump(settings, config)
