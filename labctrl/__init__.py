79e726ead382af1edc35d0cde697d110bd99a58d
Atharv Joshi <atharvjoshi@ymail.com>
2022-04-05 20:16:56 +0800

    Expose stage to outer api

--- labctrl/__init__.py
+++ labctrl/__init__.py
@@ -3,4 +3,4 @@ __version__ = "0.2.5"
 from labctrl.resource import Instrument, Resource
 from labctrl.parameter import Parameter
 from labctrl.settings import Settings
-from labctrl.stage import Stagehand
+from labctrl.stage import Stage, Stagehand
