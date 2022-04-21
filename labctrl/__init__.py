47f338deec82fbabb7f352769e1665356de9fa0d
Atharv Joshi <atharvjoshi@ymail.com>
2022-04-22 00:24:00 +0800

    Expose DataSaver to api

--- labctrl/__init__.py
+++ labctrl/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.2.5"
 
+from labctrl.datahandler import DataSaver
 from labctrl.resource import Instrument, Resource
 from labctrl.parameter import Parameter
 from labctrl.settings import Settings
