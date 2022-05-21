3fd380eccf75eca2322130e66acc2ef448be1ef4
Atharv Joshi <atharvjoshi@ymail.com>
2022-05-21 15:50:54 +0800

    Make settings attributes explicit

--- labctrl/settings.py
+++ labctrl/settings.py
@@ -1,4 +1,4 @@
-""" Control global path settings for labctrl """
+""" Control global settings for labctrl """
 
 from __future__ import annotations
 
@@ -8,39 +8,50 @@ import yaml
 
 from labctrl.logger import logger
 
-# set of all labctrl settings
-SETTINGS = {"datapath", "resourcepath"}
+SETTINGSPATH = Path.home() / ".labctrl/settings.yml"
 
-SETTINGSPATH = Path(__file__).resolve().parent / "settings.yml"
-SETTINGSPATH.touch(exist_ok=True)  # ensure an empty settings.yml always exists
-DEFAULT_SETTINGS = dict.fromkeys(SETTINGS)
-with open(SETTINGSPATH, "r+") as config:
-    if yaml.safe_load(config) is None:
-        yaml.safe_dump(DEFAULT_SETTINGS, config)  # ensure default settings always exist
+
+def make_default_settings() -> None:
+    """ensure default settings always exist"""
+    folder = SETTINGSPATH.parent
+    folder.mkdir(exist_ok=True)
+    SETTINGSPATH.touch(exist_ok=True)
+    settings = dict.fromkeys(Settings.__annotations__.keys())
+    with open(SETTINGSPATH, "r+") as config:
+        yaml.safe_dump(settings, config)
+
 
 class Settings:
-    """ 
+    """
     Context manager for settings
     How to use:
-    
-    with Settings() as settings:
-        print(settings)  # to get all labctrl settings that a user can specify
-        settings.<setting1> = <value1>  # set 'setting1' to 'value1'
-        settings.<setting2> = <value2>  # set 'setting2' to 'value2'
-        ...
+        with Settings() as settings:
+            print(settings)  # to get all labctrl settings that a user can specify
+            settings.<setting1> = <value1>  # set 'setting1' to 'value1'
+            settings.<setting2> = <value2>  # set 'setting2' to 'value2'
+            print(settings.settings)  # get latest values of all settings
+    Any unrecognized settings found in settings.yml will be ignored
     """
+
+    datapath: str
+    resourcepath: str
 
     def __init__(self) -> None:
         """ """
-        with open(SETTINGSPATH, "r") as config:
-            self._settings = yaml.safe_load(config)            
+        if not SETTINGSPATH.exists():
+            make_default_settings()
 
+        with SETTINGSPATH.open() as config:
+            self._settings = yaml.safe_load(config)
+
+        self._settings_keys = tuple(Settings.__annotations__.keys())
         for name, value in self._settings.items():
-            setattr(self, name, value)
+            if name in self._settings_keys:
+                setattr(self, name, value)
 
-    def __repr__(self) -> str:
+    def __repr__(self) -> None:
         """ """
-        return f"{self.__class__.__name__} = {SETTINGS}"
+        return f"{self.__class__.__name__}{self._settings_keys}"
 
     def __enter__(self) -> Settings:
         """ """
@@ -53,7 +64,7 @@ class Settings:
     @property
     def settings(self) -> dict[str, str]:
         """ """
-        return {setting: getattr(self, setting) for setting in SETTINGS}
+        return {setting: getattr(self, setting) for setting in self._settings_keys}
 
     def save(self) -> None:
         """ """
@@ -69,4 +80,5 @@ class Settings:
                     f"Details: {error}"
                     f"Reverted to default settings. Please fix the error and try again."
                 )
-                yaml.safe_dump(DEFAULT_SETTINGS, config)
+                default_settings = dict.fromkeys(self._settings_keys)
+                yaml.safe_dump(default_settings, config)
