f8a49c09503f7d53f95769c8baeedcc820b705e8
Atharv Joshi <atharvjoshi@ymail.com>
2022-05-21 15:47:22 +0800

    Expose Experiment, Sweep, Dataset to API
    Prepare for version 0.3.0

--- labctrl/__init__.py
+++ labctrl/__init__.py
@@ -1,7 +1,10 @@
-__version__ = "0.2.5"
+__version__ = "0.3.0"
 
-from labctrl.datahandler import DataSaver
+from labctrl.datasaver import DataSaver
+from labctrl.dataset import Dataset
+from labctrl.experiment import Experiment
 from labctrl.resource import Instrument, Resource
 from labctrl.parameter import Parameter
 from labctrl.settings import Settings
 from labctrl.stage import Stage, Stagehand
+from labctrl.sweep import Sweep
